362844b40d5e4a43bf445b603579f207a1346e88
cleanone-app <witchscoffee@naver.com>
2025-04-19 22:49:17 +0900

    Create video_generator.dart

---
+++ lib/video_generator.dart
@@ -0,0 +1,23 @@
+import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class VideoGenerator {
+  static Future<String> generateVideo({
+    required String imagePath,
+    required String audioPath,
+    required String outputFileName,
+  }) async {
+    final dir = await getTemporaryDirectory();
+    final outputPath = '${dir.path}/$outputFileName';
+
+    final command = '''
+      -loop 1 -i "$imagePath" -i "$audioPath"
+      -c:v libx264 -tune stillimage -c:a aac -b:a 192k
+      -pix_fmt yuv420p -shortest "$outputPath"
+    ''';
+
+    await FFmpegKit.execute(command);
+    return outputPath;
+  }
+}
